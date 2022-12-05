db7b2657f8db8d0f2d143fa34b7d71ded56a908e
Mantiena <mantasdagg@gmail.com>
2022-12-05 17:00:59 +0200

    created model class 'Room'

--- hotelmanagement/hotel/models.py
+++ hotelmanagement/hotel/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth. models import User
 # Create your models here.
+
+class Room(models.Model):
+    room_num = models.IntegerField()
+    guest = models.ForeignKey(User, on_delete=models.CASCADE)
+    check_in = models.DateField
+
+    def __str__(self) -> str:
+        return self.room_num
