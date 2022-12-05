98f3be63440dc7faaafd01b139ee91a0b2b1160d
Mantiena <mantasdagg@gmail.com>
2022-12-05 16:50:31 +0200

    Set local_settings

--- hotelmanagement/hotelmanagement/settings.py
+++ hotelmanagement/hotelmanagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xsb082g@5=me(4nz0ih3$9eynehpl=c$_)*$^6x2#zyo3j_j^r'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
