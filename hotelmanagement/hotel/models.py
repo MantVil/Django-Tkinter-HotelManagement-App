cb5b75db703b7b5c8b774acc462492d7dad15d8c
Mantiena <mantasdagg@gmail.com>
2022-12-05 19:21:57 +0200

    second migration completed with check-in

--- hotelmanagement/hotel/models.py
+++ hotelmanagement/hotel/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth. models import User
+from django.utils import timezone
 # Create your models here.
 
 class Room(models.Model):
     room_num = models.IntegerField()
     guest = models.ForeignKey(User, on_delete=models.CASCADE)
-    check_in = models.DateField
+    check_in = models.DateField(null=True)
+    check_out = models.DateField(null=True)
 
     def __str__(self) -> str:
         return self.room_num
